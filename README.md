d8c115e862b409c8380e8d5232c3e4bba70d1ec8
Thomas Zimmermann <tzimmermann@spirit21.com>
2023-01-21 19:31:02 +0100

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# node-red-contrib-version
-Display version information in flows
+# Version
+
+This node simply displays the entered version in the flow and sets `msg.version` to this value.
+
+It is intended to visualize the flow's version.
+
+## Nodes
+
+-   version
+    ![version node](/images/version.png)
+
+## Example:
+
+```
+[{"id":"742dd9932f8f885d","type":"group","z":"67f08dbd3ee93df0","name":"","style":{"stroke":"#d1d1d1","fill":"#ffff3f","label":true},"nodes":["0bafe7366f22cc96"],"x":434,"y":239,"w":152,"h":82},{"id":"0bafe7366f22cc96","type":"ba47-version","z":"67f08dbd3ee93df0","g":"742dd9932f8f885d","version":"1.0.0","versionType":"str","versionCalc":"","x":510,"y":280,"wires":[[]]}]
+```
+
+## Known Issues
+
+When changing the value of the version field, it must be saved twice. Looks like an event issue that may be solved at a later stage.
---
+++ src/version.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = function (RED) {
+    function versionNode(config) {
+        RED.nodes.createNode(this, config);
+        const node = this;
+
+        node.version = config.version || '';
+        node.versionType = config.versionType || 'str';
+
+        node.on('input', function (msg, send, done) {
+            let version;
+            RED.util.evaluateNodeProperty(node.version, node.versionType, node, msg, (err, value) => {
+                if (err) {
+                    version = 'version error';
+                    done(err);
+                } else version = value;
+            });
+
+            // node.context().global.set('version', version);
+            msg.version = version;
+
+            send(msg);
+            done();
+        });
+
+        RED.httpAdmin.get('/environ-val/:key', function (req, res) {
+            res.json({ value: process.env[req.params.key] });
+        });
+    }
+
+    RED.nodes.registerType('ba47-version', versionNode);
+};
